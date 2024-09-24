326e320b7eac685bae0acdf90fc15abebc437da1
jagannath p s <110271239+jagannath-p-s@users.noreply.github.com>
2024-09-24 21:19:31 +0530

    Update Members.jsx

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import jsPDF from 'jspdf';
-import domtoimage from 'dom-to-image';
+import domtoimage from 'dom-to-image-more';
 import {
   Container,
   Paper,
